e89d7a099b8bada4755be241c2297ef1a8814c1e
Hailey Dangel <Hailey Dangel>
2017-01-15 12:29:27 -0800

    .

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,24 @@
 console.log("JS file is connected to HTML! Woo!")
+var cardOne = "Queen";
+var cardTwo = "King";
+var cardThree = "Queen";
+var cardFour = "King";
+
+/* if cardTwo === cardFour {
+	alert: "You found a match!"
+}		if else cardOne === cardThree {
+	alert: "You found a match!"
+}		if else cardOne === cardFour {
+	alert: "Sorry, try again."
+}		 else {
+	alert: "Sorry, try again."} */
+
+var createCards = function(){
+	var gameBoard = document.getElementById('game-board');
+	for ( var i = 0; i <4; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	}
+}
+createCards();
